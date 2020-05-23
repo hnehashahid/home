be64c9ebbc5eac6f427207eb9032c169242fabe5
Hafiza Neha Shahid <neha.shahid1398@gmail.com>
2020-05-23 23:07:21 +0500

    Updates

---
+++ precache-manifest.01d2fd3886b010bc63f0b4bbd10b6ba5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9e84e2cff065dfd21551b8072345fa",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/home/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d07bf12d96101364b6e6",
+    "url": "/home/static/css/main.307a871c.chunk.css"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/home/static/js/2.448dd0be.chunk.js"
+  },
+  {
+    "revision": "b71cb3cccc73e099d412c24a04bf9105",
+    "url": "/home/static/js/2.448dd0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d07bf12d96101364b6e6",
+    "url": "/home/static/js/main.9bf31100.chunk.js"
+  },
+  {
+    "revision": "5c07ecca0456e28e04f0",
+    "url": "/home/static/js/runtime-main.e084aacb.js"
+  },
+  {
+    "revision": "e5a2206d39587737cfd6759a32e0bd57",
+    "url": "/home/static/media/resume.e5a2206d.pdf"
+  }
+]);
