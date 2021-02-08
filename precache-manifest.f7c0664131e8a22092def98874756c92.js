dfb87648d03add72acefd4227b4da73caad9311d
Hafiza Neha Shahid <neha.shahid1398@gmail.com>
2021-02-08 15:23:40 +0500

    Updates

--- precache-manifest.f7c0664131e8a22092def98874756c92.js
+++ precache-manifest.f7c0664131e8a22092def98874756c92.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6bf83179e77685e1ddad355904505a24",
+    "revision": "6101763ebfbb86e32de5beac7baed24c",
     "url": "/home/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/home/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "b5463ede7f23d8f97723",
+    "revision": "fbb50e8a970bfa615322",
     "url": "/home/static/css/main.307a871c.chunk.css"
   },
   {
@@ -20,15 +20,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/home/static/js/2.448dd0be.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b5463ede7f23d8f97723",
-    "url": "/home/static/js/main.6a450f7d.chunk.js"
+    "revision": "fbb50e8a970bfa615322",
+    "url": "/home/static/js/main.ae06b99a.chunk.js"
   },
   {
     "revision": "5c07ecca0456e28e04f0",
     "url": "/home/static/js/runtime-main.e084aacb.js"
   },
   {
-    "revision": "71d30312e933d32e3b6cf3476d0b5844",
-    "url": "/home/static/media/resume.71d30312.pdf"
+    "revision": "013bcb9b9b6c12d44fce0fbe9a6348a1",
+    "url": "/home/static/media/resume.013bcb9b.pdf"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.3fe01022e18ad2d876279eb5bb45579a.js"
+  "/home/precache-manifest.f7c0664131e8a22092def98874756c92.js"
 );
 
 self.addEventListener('message', (event) => {
